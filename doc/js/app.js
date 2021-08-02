cef9b01a7e0ac218746039eb687bbffb192f7da4
maxhousedev <max.msp83@gmail.com>
2021-08-02 08:19:25 +0300

    Delete app.js

--- doc/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { createApp } from './studentsDatabase.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const MOUNT = document.getElementById('root');
-  createApp(MOUNT);
-});
